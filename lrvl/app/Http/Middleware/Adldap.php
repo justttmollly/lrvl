da1eb450b23e4f1e4a9fe2d1a1355a1abf48aed6
m.abdrakhmanova <m.abdrakhmanova@niikmg.kz>
2021-07-22 16:09:46 +0600

    SET

--- lrvl/app/Http/Middleware/Adldap.php
+++ lrvl/app/Http/Middleware/Adldap.php
@@ -3,19 +3,51 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Helpers\Helper;
 use Illuminate\Http\Request;
 
 class Adldap
 {
+
+    public $attributes;
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @return mixed
+     *
      */
+
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        $authenticated = false;
+
+        if ($email = $request->getUser() && $password = $request->getPassword()) {
+            Log::info("Authenticate user using basic auth");
+            $credentials = array(
+                'samaccountname' => $request->getUser(),
+                'password' => $request->getPassword()
+            );
+            //
+            $authenticated = $this->authLdapUser($credentials);
+
+            if($authenticated) {
+                $user = $this->getLdapUser($request->getUser());
+            }
+        }
+        if (!empty($user)) {
+            $authenticated = true;
+            if(!$this->checkRoles($user['groups'], $role)) {
+                return response("User is not authorize for this request.", 401);
+            }
+            $request->attributes->add(['user_groups' => $user['groups']]);
+            return $next($request);
+        }
+
+        if (!$authenticated) {
+            return response("Unauthorized: Access is denied due to invalid credentials.", 401);
+        }
     }
+        //return $next($request);
 }
