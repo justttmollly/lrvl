c1acf0561ef6a768b43bbaa0e5e6e04efb8961ef
m.abdrakhmanova <m.abdrakhmanova@niikmg.kz>
2021-07-23 15:39:58 +0600

    socialite of microsoft

--- lrvl/app/Http/Middleware/Adldap.php
+++ lrvl/app/Http/Middleware/Adldap.php
@@ -8,8 +8,7 @@ use Illuminate\Http\Request;
 
 class Adldap
 {
-//  use Helper;
-    public $attributes;
+
     /**
      * Handle an incoming request.
      *
@@ -21,33 +20,7 @@ class Adldap
 
     public function handle(Request $request, Closure $next)
     {
-        $authenticated = false;
 
-        if ($email = $request->getUser() && $password = $request->getPassword()) {
-            Log::info("Authenticate user using basic auth");
-            $credentials = array(
-                'samaccountname' => $request->getUser(),
-                'password' => $request->getPassword()
-            );
-            //
-            $authenticated = $this->authLdapUser($credentials);
-
-            if($authenticated) {
-                $user = $this->getLdapUser($request->getUser());
-            }
-        }
-        if (!empty($user)) {
-            $authenticated = true;
-            if(!$this->checkRoles($user['groups'], $role)) {
-                return response("User is not authorize for this request.", 401);
-            }
-            $request->attributes->add(['user_groups' => $user['groups']]);
-            return $next($request);
-        }
-
-        if (!$authenticated) {
-            return response("Unauthorized: Access is denied due to invalid credentials.", 401);
-        }
+        return $next($request);
     }
-        //return $next($request);
 }
