6946f133fbf58074b9e066184ca7ea3b14b4f715
m.abdrakhmanova <m.abdrakhmanova@niikmg.kz>
2021-07-26 12:03:25 +0600

    conf

--- lrvl/config/services.php
+++ lrvl/config/services.php
@@ -20,6 +20,13 @@ return [
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
+
+    'microsoft' => [
+        'client_id' => env('MICROSOFT_CLIENT_ID'),
+        'client_secret' => env('MICROSOFT_CLIENT_SECRET'),
+        'redirect' => env('MICROSOFT_REDIRECT_URI')
+    ],
+
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
